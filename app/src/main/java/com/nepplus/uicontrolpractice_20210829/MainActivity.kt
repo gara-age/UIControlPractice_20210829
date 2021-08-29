97e188f74344eaab75a4bdc0b292985fc07b677d
최민서 <minseo00515@naver.com>
2021-08-29 12:33:12 +0900

    set 기능 체험용 UI 작업

--- app/src/main/java/com/nepplus/uicontrolpractice_20210829/MainActivity.kt
+++ app/src/main/java/com/nepplus/uicontrolpractice_20210829/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 //        토스트버튼이 눌리면 => 메시지가 적힌 내용을 받아와서 => 토스트를 띄우는데 사용
 
         toastBtn.setOnClickListener {
-//            메시지가 입력된 내용을 받아오자 => 컴퓨터가 기억해둬야 => 변수를 만들어서 저장해두자.
+//            메시지가 입력된 내용(text 속성에 적힌값)을 받아오자 => 컴퓨터가 기억해둬야 => 변수를 만들어서 저장해두자.
             val inputMessage = messageEdt.text.toString()
 //            토스트를 띄울 때 변수에 담긴 내용을 띄우자
             Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
